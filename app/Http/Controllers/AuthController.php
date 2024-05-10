7625803b4224636b91477291199038f779b8e362
pramod <pramoddewangan128@gmail.com>
2024-05-10 14:07:55 +0530

    inor change

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,6 +11,7 @@ class AuthController extends Controller
     public function showRegistrationForm()
     {
         return view('register');
+        
     }
 
     public function register(Request $request)
